a1b7ef7e354f08235d912d292375d15dec9d24fd
topsailRobotics <topsailroboticsteam@gmail.com>
2025-12-05 16:39:38 -0500

    Added a for loop

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(String[] args) {
         System.out.print("Hello World!");
+
+        for (int i = 0; i < args.length; i++){
+            System.out.print("You said to make this text appear this many times");
+        }
     }
 }
