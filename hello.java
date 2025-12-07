2d308cb1a7bbbe95f6e78b8ae4d0a76215ebb7f9
shligls <154032989+shligls@users.noreply.github.com>
2025-12-06 22:21:15 -0500

    Added the print command saying where it was written -shligls

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String[] args) {
         System.out.print("Hello World!");
-
+        System.out.print("I wrote this in GitHub!");
         for (int i = 0; i < args.length; i++){
             System.out.print("You said to make this text appear this many times");
         }
