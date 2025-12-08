4652d4c30edc7bcc82f11470ab846c438ff32264
shligls <parkerwilliam0731@gmail.com>
2025-12-07 21:22:08 -0500

    Fixed first for loop, and added scenond for loop
    The first for loop used the length of the args instead of the value inputed.

--- hello.java
+++ hello.java
@@ -1,9 +1,15 @@
 public class hello{
     public static void main(String[] args) {
+        final int lung = 0;
+
         System.out.print("Hello World!");
         System.out.print("I wrote this in GitHub!");
-        for (int i = 0; i < args.length; i++){
+
+        for (int i = 0; i < Integer.parseInt(args[0]); i++){
             System.out.print("You said to make this text appear this many times");
+        }
+        for (int i = 0; i < Integer.parseInt(args[1]); i++){
+            System.out.print("You also said to make this text appear this many times");
         }
     }
 }
